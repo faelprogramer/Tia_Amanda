1c302e54fb5beb63ce068bf628d918468d249cd0
Fael <Fael@10.0.0.11>
2017-04-13 19:10:33 -0300

    Troca de servidor postgrees

--- src/br/com/tiaAmanda/modelo/bean/dao/factory/PostgresConnectionFactory.java
+++ src/br/com/tiaAmanda/modelo/bean/dao/factory/PostgresConnectionFactory.java
@@ -10,7 +10,7 @@ public final class PostgresConnectionFactory extends ConnectionFactory {
         user = "postgres";
         password = "admin";
         db = "acai";
-        host = "10.0.0.10";
+        host = "10.0.0.12";
         port = "5432";
         driver = "org.postgresql.Driver";
         url = getUrlConnection();
